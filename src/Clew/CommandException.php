5039c499e8c59edbd6afea0f09dde94b14ad9990
KITO, takayuki <takayuki.kito@uhero.co.jp>
2018-05-17 15:51:27 +0900

    Add properties: commandLine, result

--- src/Clew/CommandException.php
+++ src/Clew/CommandException.php
@@ -6,4 +6,45 @@ use Exception;
 
 class CommandException extends Exception
 {
+    /**
+     * @var string
+     */
+    private $commandLine;
+
+    /**
+     * @var CommandResult
+     */
+    private $result;
+
+    /**
+     * @param string $commandLine
+     */
+    public function setCommandLine($commandLine)
+    {
+        $this->commandLine = $commandLine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCommandLine()
+    {
+        return $this->commandLine;
+    }
+
+    /**
+     * @param CommandResult $result
+     */
+    public function setCommandResult(CommandResult $result)
+    {
+        $this->result = $result;
+    }
+
+    /**
+     * @return CommandResult
+     */
+    public function getCommandResult()
+    {
+        return $this->result;
+    }
 }
